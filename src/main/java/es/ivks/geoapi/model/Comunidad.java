346212ee83330e419a1f881cbe6eb58612c5a61d
Guillem Casas <gucasassi@gmail.com>
2021-02-12 00:05:34 +0100

    Create Comunidad Model. Closes #4

---
+++ src/main/java/es/ivks/geoapi/model/Comunidad.java
@@ -0,0 +1,22 @@
+package es.ivks.geoapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: guillem.casas
+ * @version: 12/02/2021
+**/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comunidad {
+
+    private Long id;
+    private String name;
+    private String updated;
+
+}
